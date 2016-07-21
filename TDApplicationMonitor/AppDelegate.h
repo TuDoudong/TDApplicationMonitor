5516a3ae66dd4c221341a62014dbdfd7ed535a7d
TuDoudong <920959054@qq.com>
2016-07-21 14:56:51 +0800

    项目初始化

---
+++ TDApplicationMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TDApplicationMonitor
+//
+//  Created by 董慧翔 on 16/7/21.
+//  Copyright © 2016年 TudouDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TDApplicationMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TDApplicationMonitor
+//
+//  Created by 董慧翔 on 16/7/21.
+//  Copyright © 2016年 TudouDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
